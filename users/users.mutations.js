d17e0990c3dfcd82b33878829159f98f69d3ac02
denev <denev723@gmail.com>
2021-08-18 06:02:59 +0900

    see Profile

--- users/users.mutations.js
+++ users/users.mutations.js
@@ -7,17 +7,29 @@ export default {
       _,
       { firstName, lastName, username, email, password },
     ) => {
-      // 유저네임이나 이메일이 DB에 이미 존재하는지 확인
-      const existingUser = await client.user.findFirst({
-        where: { OR: [{ username }, { email }] },
-      });
-      console.log(existingUser);
-      // 패스워드 해쉬
-      const uglyPassword = await bcrypt.hash(password, 10);
-      // 유저정보 저장 및 유저 리턴
-      return client.user.create({
-        data: { username, email, firstName, lastName, password: uglyPassword },
-      });
+      try {
+        // 유저네임이나 이메일이 DB에 이미 존재하는지 확인
+        const existingUser = await client.user.findFirst({
+          where: { OR: [{ username }, { email }] },
+        });
+        if (existingUser) {
+          throw new Error("This username or password is already taken.");
+        }
+        // 패스워드 해쉬
+        const uglyPassword = await bcrypt.hash(password, 10);
+        // 유저정보 저장 및 유저 리턴
+        return client.user.create({
+          data: {
+            username,
+            email,
+            firstName,
+            lastName,
+            password: uglyPassword,
+          },
+        });
+      } catch (error) {
+        return error;
+      }
     },
   },
 };
